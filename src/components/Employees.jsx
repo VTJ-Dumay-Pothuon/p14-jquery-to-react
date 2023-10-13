d504353cf73693b0a25f3bc0ad43ecee5e16355a
VTJ-Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2023-10-13 19:49:29 +0200

    created page

---
+++ src/components/Employees.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom'
+/* global $ */
+
+function Employees() {
+  useEffect(() => {
+    const employees = JSON.parse(localStorage.getItem('employees'));
+
+    $(document).ready(function () {
+      $('#employee-table').DataTable({
+        data: employees,
+        columns: [
+          { title: 'First Name', data: 'firstName' },
+          { title: 'Last Name', data: 'lastName' },
+          { title: 'Start Date', data: 'startDate' },
+          { title: 'Department', data: 'department' },
+          { title: 'Date of Birth', data: 'dateOfBirth' },
+          { title: 'Street', data: 'street' },
+          { title: 'City', data: 'city' },
+          { title: 'State', data: 'state' },
+          { title: 'Zip Code', data: 'zipCode' },
+        ],
+      });
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Current Employees</h1>
+      <table id="employee-table" className="display"></table>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
+
+export default Employees;
