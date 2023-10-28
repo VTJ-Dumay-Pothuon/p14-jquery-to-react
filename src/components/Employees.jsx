ab372c0013680a85b3332c05e8d7a2151af9ee9c
VTJ-Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2023-10-29 01:03:01 +0200

    Completed table logic (only search missing)

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -2,45 +2,51 @@ import { useState, useEffect, useMemo } from 'react'
 import TableTemplate from './TableTemplate'
 import { Link } from 'react-router-dom'
 
-import '../styles/Employees.css'
+import '../styles/Employees.scss'
 
 const Employees = () => {
-  const [data, setData] = useState([])
+    const [data, setData] = useState([])
 
-  const alphabetical = useMemo(() => (rowA, rowB, columnId) => {
-    const a = rowA.original[columnId].toLowerCase()
-    const b = rowB.original[columnId].toLowerCase()
-    return (a > b) ? 1 : -1
-  }, [])
-  
-  const columns = [
-    { Header: 'First Name',    accessor: 'firstName',  sortType: alphabetical   },
-    { Header: 'Last Name',     accessor: 'lastName',   sortType: alphabetical   },
-    { Header: 'Start Date',    accessor: 'startDate',  sortType: 'datetime'     },
-    { Header: 'Department',    accessor: 'department', sortType: alphabetical   },
-    { Header: 'Date of Birth', accessor: 'dateBirth',  sortType: 'datetime'     },
-    { Header: 'Street',        accessor: 'street',     sortType: 'alphanumeric' },
-    { Header: 'City',          accessor: 'city',       sortType: alphabetical   },
-    { Header: 'State',         accessor: 'state',      sortType: alphabetical   },
-    { Header: 'Zip Code',      accessor: 'zipCode',    sortType: 'alphanumeric' }
-  ]
+    const alphabetical = useMemo(() => (rowA, rowB, columnId) => {
+        const a = rowA.original[columnId].toLowerCase()
+        const b = rowB.original[columnId].toLowerCase()
+        return (a > b) ? 1 : -1
+    }, [])
 
-  useEffect(() => {
-    const employees = JSON.parse(localStorage.getItem('employees'))
-    if (employees) {
-      setData(employees)
-    }
-  }, [])
+    const datetime = useMemo(() => (rowA, rowB, columnId) => {
+        const a = new Date(rowA.original[columnId])
+        const b = new Date(rowB.original[columnId])
+        return (a > b) ? 1 : -1
+    }, [])
 
-  return (
-    <div className="App">
-      <h1 className='title'>Current Employees</h1>
-      <section className="container">
-        <TableTemplate columns={columns} data={data} />
-        <Link to="/">Home</Link>
-      </section>
-    </div>
-  )
+    const columns = [
+  { Header: 'First Name',   accessor: 'firstName',  sortType: alphabetical },
+  { Header: 'Last Name',    accessor: 'lastName',   sortType: alphabetical },
+  { Header: 'Start Date',   accessor: 'startDate',  sortType: datetime     },
+  { Header: 'Department',   accessor: 'department', sortType: alphabetical },
+  { Header: 'Date of Birth',accessor: 'dateOfBirth',sortType: datetime     },
+  { Header: 'Street',       accessor: 'street',     sortType: alphabetical },
+  { Header: 'City',         accessor: 'city',       sortType: alphabetical },
+  { Header: 'State',        accessor: 'state',      sortType: alphabetical },
+  { Header: 'Zip Code',     accessor: 'zipCode',    sortType: alphabetical }
+    ]
+
+    useEffect(() => {
+        const employees = JSON.parse(localStorage.getItem('employees'))
+        if (employees) {
+            setData(employees)
+        }
+    }, [])
+
+    return (
+        <div className="App">
+            <h1 className='title'>Current Employees</h1>
+            <section className="container">
+                <TableTemplate columns={columns} data={data}/>
+                <Link to="/">Home</Link>
+            </section>
+        </div>
+    )
 }
 
 export default Employees
--- src/components/TableTemplate.jsx
+++ src/components/TableTemplate.jsx
@@ -2,118 +2,193 @@ import { useEffect } from 'react'
 import { useTable, useSortBy, usePagination } from 'react-table'
 import PropTypes from 'prop-types'
 
-  const TableTemplate = ({ columns, data }) => {
-     const {
-       getTableProps,
-       getTableBodyProps,
-       headerGroups,
-       prepareRow,
-       page,
-       nextPage,
-       previousPage,
-       canNextPage,
-       canPreviousPage,
-       state: { pageIndex, pageSize, sortBy },
-       setPageSize
-     } = useTable(
-       {
-         columns,
-         data,
-         initialState: { pageIndex: 0, pageSize: 10, sortBy: [{ id: 'firstName', desc: false }] }
-       },
-       useSortBy,
-       usePagination
-     )
+const TableTemplate = ({columns, data}) => {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page,
+        gotoPage,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage,
+        state: { pageIndex, pageSize, sortBy},
+        setPageSize
+    } = useTable({
+        columns,
+        data,
+        initialState: { pageIndex: 0, pageSize: 10, 
+          sortBy: [{ id: 'firstName', desc: false }]
+        }
+    }, useSortBy, usePagination)
 
     useEffect(() => {
-      if (sortBy.length > 0) {
-        const sortedColumn = columns.find((column) => column.accessor === sortBy[0].id)
-        if (sortedColumn) {
-          document.querySelectorAll('.sorted').forEach((element) => {
-            element.classList.remove('sorted')
-          })
+        if (sortBy.length > 0) {
+            const sortedColumn = columns.find((column) => column.accessor === sortBy[0].id)
+            if (sortedColumn) {
+                document.querySelectorAll('.sorted').forEach((element) => {
+                        element.classList.remove('sorted')
+                    })
 
-          const index = columns.findIndex((column) => column.accessor === sortBy[0].id) + 1
+                const index = columns.findIndex((column) => column.accessor === sortBy[0].id) + 1
 
-          const headerElements = Array.from(document.querySelectorAll('thead tr th:nth-child(' + index + ')'));
-          const cellElements = Array.from(document.querySelectorAll('tbody tr td:nth-child(' + index + ')'));
-          const headerAndCells = headerElements.concat(cellElements);
-
-          console.log(headerAndCells)
-          headerAndCells.forEach((element) => {
-            element.classList.add('sorted')
-          })
+                const headerElements = Array.from(document.querySelectorAll('thead tr th:nth-child(' + index + ')'));
+                const cellElements = Array.from(document.querySelectorAll('tbody tr td:nth-child(' + index + ')'));
+                const headerAndCells = headerElements.concat(cellElements);
+                headerAndCells.forEach((element) => {
+                    element.classList.add('sorted')
+                })
+            }
         }
-      }
     }, [sortBy, columns])
-    
-     return (
-       <div>
-         <div className="entries">
-            <span>Show </span>
-              <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
-                {[5, 10, 25, 50].map(pageSize => (
-                  <option key={pageSize} value={pageSize}>
-                    {pageSize}
-                  </option>
-                ))}
-              </select>
-            <span> entries</span>
-         </div>
-         <table {...getTableProps()} className="table">
-           <thead>
-             {headerGroups.map(headerGroup => (
-               <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
-                 {headerGroup.headers.map(column => (
-                   <th {...column.getHeaderProps(column.getSortByToggleProps())} key={column.id}>
-                     {column.render('Header')}
-                     <span>
-                       {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
-                     </span>
-                   </th>
-                 ))}
-               </tr>
-             ))}
-           </thead>
-          <tbody {...getTableBodyProps()}>
-            {page.map(row => {
-              prepareRow(row)
-              return (
-                <tr {...row.getRowProps()} key={row.id}>
-                  {row.cells.map(cell => {
-                    return <td {...cell.getCellProps()} key={cell.column.id}>{cell.render('Cell')}</td>
-                  })}
-                </tr>
-              )
-            })}
-          </tbody>
 
-         </table>
-         <div className="entries">
-            <span>Showing {pageIndex * pageSize + 1} to {pageIndex * 
-            pageSize + page.length} of {data.length} entries</span>
-         </div>
-         <div className="pagination">
-           <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-             Previous
-           </button>
-           <span>
-             Page{' '}
-             <strong>
-               {pageIndex + 1} of {Math.ceil(data.length / pageSize)}
-             </strong>{' '}
-           </span>
-           <button onClick={() => nextPage()} disabled={!canNextPage}>
-             Next
-           </button>
-         </div>
-       </div>
-     )
-   }
-
-   TableTemplate.propTypes = {
-      columns: PropTypes.array.isRequired,
-      data: PropTypes.array.isRequired,
+    const hideButtons = () => {
+        const buttons = document.querySelectorAll('.pagination button')
+        if (buttons.length < 12) {
+            buttons.forEach((button, index) => {
+                button.style.display = ''
+                if (index === 0) {
+                    button.textContent = "Previous"
+                } else if (index === buttons.length - 1) {
+                    button.textContent = "Next"
+                } else {
+                    button.textContent = index
+                    button.disabled = false
+                }
+            })
+            return
+        }
+        const currentPage = document.querySelector('.page--current')
+        buttons.forEach((button, index) => {
+            button.style.display = ''
+            if (index !== 0 && index !== 1 && button !== currentPage.previousElementSibling && button !== currentPage && button !== currentPage.nextElementSibling && index !== buttons.length - 2 && index !== buttons.length - 1) {
+                button.textContent = '…'
+                button.disabled = true
+                if (buttons[index - 1].textContent === '…') {
+                    button.style.display = 'none'
+                }
+            } else if (index === 0) {
+                button.textContent = "Previous"
+            } else if (index === buttons.length - 1) {
+                button.textContent = "Next"
+            } else {
+                button.textContent = index
+                button.disabled = false
+            }
+        })
     }
 
-   export default TableTemplate
+    useEffect(() => {
+        hideButtons()
+    }, [pageSize, pageIndex])
+
+    window.addEventListener('load', () => {
+        hideButtons()
+        window.removeEventListener('load', () => {})
+    })
+
+    return (
+        <div>
+            <div className="tableHeader">
+                <div className="entries">
+                    <span>Show </span>
+                    <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
+                        {[5, 10, 25, 50].map(pageSize => (
+                            <option key={pageSize} value={pageSize}>
+                                {pageSize}
+                            </option>
+                        ))}
+                    </select>
+                    <span> entries</span>
+                </div>
+                <div className="search">
+                  <span>Search: </span>
+                  <input type="text"/>
+                </div>
+            </div>
+            <table {...getTableProps()} className="table">
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+                            {headerGroup
+                                .headers
+                                .map(column => (
+                                    <th {...column.getHeaderProps(column.getSortByToggleProps())} key={column.id}>
+                                        {column.render('Header')}
+                                        <span className='sortButton'>
+                                            {column.isSorted
+                                                ? (column.isSortedDesc
+                                                    ? <img
+                                                            src="https://img.icons8.com/ios-filled/15/7A80DD/sort-down.png"
+                                                            alt="sort down"/>
+                                                    : <img
+                                                        src="https://img.icons8.com/ios-filled/15/DD7A7E/sort-up.png"
+                                                        alt="sort up"/>)
+                                                : <img src="https://img.icons8.com/ios-filled/15/DCDCDC/sort.png" alt="sort"/>}
+                                        </span>
+                                    </th>
+                                ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()} key={row.id}>
+                                {row
+                                    .cells
+                                    .map(cell => {
+                                        return <td {...cell.getCellProps()} key={cell.column.id}>{cell.render('Cell')}</td>
+                                    })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+
+            </table>
+            <div className="tableFooter">
+                <div className="entries">
+                    <span>Showing {pageIndex * pageSize + 1}
+                        to {pageIndex * pageSize + page.length}
+                        of {data.length}
+                        entries</span>
+                </div>
+                <div className="pagination">
+                    <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                        Previous
+                    </button>
+                    {Array.from({
+                        length: Math.ceil(data.length / pageSize)
+                    }, (_, i) => i + 1).map(buttonIndex => (
+                        <button
+                            key={buttonIndex}
+                            id={`page-${buttonIndex}`}
+                            className={pageIndex + 1 === buttonIndex
+                            ? 'page--current'
+                            : ''}
+                            onClick={() => gotoPage(buttonIndex - 1)}>
+                            {buttonIndex}
+                        </button>
+                    ))}
+                    <button
+                        onClick={() => {
+                        nextPage()
+                    }}
+                        disabled={!canNextPage}>
+                        Next
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+TableTemplate.propTypes = {
+    columns: PropTypes.array.isRequired,
+    data: PropTypes.array.isRequired
+}
+
+export default TableTemplate
